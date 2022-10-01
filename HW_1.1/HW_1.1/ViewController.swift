099822bdbb7b269a546f5fa97a719b4ef30b9b7a
MikeO <mike@gmail.com>
2022-10-02 05:37:08 +0700

    first version

---
+++ HW_1.1/HW_1.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_1.1
+//
+//  Created by Mishanya on 02.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
